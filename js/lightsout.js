6a527601d95f9c66e48bf80a14795dbd21f50afa
Oliver Lau <oliver@von-und-fuer-lau.de>
2014-02-17 08:06:13 +0100

    `playSolution()` spielt Lösung nun von links oben nach rechts unten ab, nicht mehr zufällig.

--- js/lightsout.js
+++ js/lightsout.js
@@ -507,17 +507,7 @@ var Solver = (function () {
               moves.push({ x: x, y: y });
           }
         }
-        function shuffleArray(array) {
-          var i, j, temp;
-          for (i = array.length - 1; i > 0; --i) {
-            j = Math.floor(Math.random() * (i + 1));
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-          }
-          return array;
-        }
-        return shuffleArray(moves);
+        return moves;
       })(),
       makeTurn = function () {
         if (i < flips.length) {
